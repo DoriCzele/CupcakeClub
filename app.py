1fcde734da560eeed5c1c16edde867d56b9a3069
DoriCzele <dorottya.czele@gmail.com>
2022-02-28 20:58:26 +0000

    Create navigation bar

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# http://localhost:8000/base
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+
+@app.route("/base")
+def temp_base_test():
+    return render_template("layout/base.html")
 
 
 if __name__ == "__main__":
