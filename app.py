b3946a632d28972edd851edacf98c28bcba9973e
DoriCzele <dorottya.czele@gmail.com>
2022-03-02 16:33:39 +0000

    Add 403 error page

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def temp_base_test():
     return render_template("layout/base.html")
 
 
+@app.route("/403")
+def temp_403_test():
+    return render_template("layout/403.html")
+
+
 if __name__ == "__main__":
     app.run(host="localhost", port="8000", debug=True)
