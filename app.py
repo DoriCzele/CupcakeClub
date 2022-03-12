50d5e31abc687808b4919691ce49d278c6df17e6
DoriCzele <dorottya.czele@gmail.com>
2022-03-12 14:28:04 +0000

    Pass url parameter: next between views

--- app.py
+++ app.py
@@ -55,9 +55,19 @@ def page_server_error(error):
 def home():
     return render_template("pages/home.html")
 
+def get_auth_url_params(request, current_auth_link, alternative_auth_link):
+    if request.args.get("next"):
+        next_endpoint = request.args.get("next")
+        current_auth_link = f"{current_auth_link}?next={next_endpoint}"
+        alternative_auth_link = f"{alternative_auth_link}?next={next_endpoint}"
+    return [next_endpoint, current_auth_link, alternative_auth_link]
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    current_auth_link = url_for("login")
+    alternative_auth_link = url_for("register")
+    next_endpoint = None
+    [next_endpoint, current_auth_link, alternative_auth_link] = get_auth_url_params(request, current_auth_link, alternative_auth_link)
     if bool("user" in session):
         flash(ALREADY_AUTHED_MESSAGE)
         return redirect(url_for("home"))
@@ -68,10 +78,12 @@ def login():
             db_user = pymongo.db.users.find_one({"username": username})
             if db_user is None:
                 flash("This user does not exist. Please check your details and try again.")
-                return redirect(url_for("login"))
+                return redirect(current_auth_link)
             if check_password_hash(db_user["password"], password):
                 session["user"] = str(db_user["_id"])
                 flash(f"Welcome back {username}!")
+                if next_endpoint:
+                    return redirect(url_for(next_endpoint))
                 return redirect(url_for("home"))
             flash("Incorrect credentials. Please check your details and try again.")
         except Exception as exception:
@@ -79,15 +91,19 @@ def login():
     return render_template(
         "pages/authentication.html",
         auth_mode="Login",
-        current_auth_link=url_for("login"),
+        current_auth_link=current_auth_link,
         alternative_auth_mode="Register",
         alternative_auth_prompt="Create an account",
-        alternative_auth_link=url_for("register"),
+        alternative_auth_link=alternative_auth_link,
     )
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    current_auth_link = url_for("register")
+    alternative_auth_link = url_for("login")
+    next_endpoint = None
+    [next_endpoint, current_auth_link, alternative_auth_link] = get_auth_url_params(request, current_auth_link, alternative_auth_link)
     if bool("user" in session):
         flash(ALREADY_AUTHED_MESSAGE)
         return redirect(url_for("home"))
@@ -103,20 +119,28 @@ def register():
                     {"username": username}
                 )
                 if not username_already_exists:
-                    pymongo.db.users.insert_one(user)
-                    flash("Welcome to The Cupcake Club!")
-                    return redirect(url_for("home"))
+                    db_insert = pymongo.db.users.insert_one(user)
+                    # If insert into database is successful, add user to session and redirect
+                    if db_insert.acknowledged:
+                        session["user"] = str(db_insert.inserted_id)
+                        flash("Welcome to The Cupcake Club!")
+                        if next_endpoint:
+                            return redirect(url_for(next_endpoint))
+                        return redirect(url_for("home"))
+                    else:
+                        flash(GENERIC_ERROR_MESSAGE)
                 else:
                     flash("That username is taken, please try another.")
         except Exception as exception:
+            print(exception)
             flash(GENERIC_ERROR_MESSAGE)
     return render_template(
         "pages/authentication.html",
         auth_mode="Register",
-        current_auth_link=url_for("register"),
+        current_auth_link=current_auth_link,
         alternative_auth_mode="Login",
         alternative_auth_prompt="Already a member?",
-        alternative_auth_link=url_for("login"),
+        alternative_auth_link=alternative_auth_link,
     )
 
 
@@ -130,7 +154,7 @@ def logout():
 @app.route("/create-recipe", methods=["GET", "POST"])
 def new_recipe():
     if not bool("user" in session):
-        return redirect(url_for("login"))
+        return redirect(url_for("login", next=request.endpoint))
     if request.method == "POST":
         try:
             recipe_name = request.form.get("name").lower()
